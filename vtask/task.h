fe1c2fcde7292f9f13d2b2f0528cfebf86c4493b
shenyang <aphx1988@qq.com>
2020-09-22 23:39:29 +0800

    qwqw

--- vtask/task.h
+++ vtask/task.h
@@ -1,19 +1,53 @@
 #pragma once
 #include <thread>
+#include <functional>
 #include "../vcontainer/vec.h"
 
 namespace valkyr {
 
 	using Worker = std::thread;
 
-	template <class F>
+	template <typename ...Args>
 	struct Task {
-		F func;
+		std::function<void(Args...)> func;
+
+		Task(std::function<void(Args...)> f) {
+			func = f;
+		}
+
+
 	};
 
-	class Scheduler {
+	template <typename Task_t>
+	struct TaskGroup {
+		std::atomic<unsigned int> completedCount;
+		std::vector<Task_t> tasks;
+	};
+
+	class TaskSys {
+	public:
+		void Init() {
+			m_maxWorkers = std::thread::hardware_concurrency();
+			for (int i = 1; i < m_maxWorkers; i++) {
+				m_workers.push_back(Worker([]() {
+
+				}));
+			}
+		}
+
+		template <typename Task_t>
+		void Exec(TaskGroup<Task_t>& group) {
+			for (auto it = m_freeWorkerNos.begin(); it != m_freeWorkerNos.end(); it++) {
+				m_workers[it].detach();
+			}
+		}
+
+		int m_maxWorkers;
+
 	private:
-		Vec<Worker> workers;
-		Vec<unsigned int> freeWorkerIds;
+		Vec<Worker> m_workers;
+		Vec<int> m_freeWorkerNos;
+
+
 	};
 }
