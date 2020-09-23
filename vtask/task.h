0596117c6e0020bb10753cadcd4759a3f64c9767
shenyang <aphx1988@qq.com>
2020-09-23 17:42:40 +0800

    weqweqweqw

--- vtask/task.h
+++ vtask/task.h
@@ -1,42 +1,50 @@
 #pragma once
 #include <thread>
 #include <functional>
+#include <any>
 #include "../vcontainer/vec.h"
 
 namespace valkyr {
 
-	using Worker = std::thread;
-
-	template <typename ...Args>
 	struct Task {
-		std::function<void(Args...)> func;
+		std::function<void(Vec<std::any>)> fun;
+		Vec<std::any> params;
 
-		Task(std::function<void(Args...)> f) {
-			func = f;
+		Task(std::function<void(Vec<std::any>)> f) {
+			fun = f;
 		}
-
-
 	};
 
-	template <typename Task_t>
 	struct TaskGroup {
 		std::atomic<unsigned int> completedCount;
-		std::vector<Task_t> tasks;
+		Vec<Task> tasks;
 	};
 
+	struct Worker {
+		bool free;
+		bool neverSleep;
+
+		void doWork(Task task) {
+			free = false;
+			task.fun(task.params);
+			free = true;
+			if(!neverSleep)
+				std::this_thread::yield();
+		}
+	};
+
+	//小心子线程直接执行或detach后，主线程退出时子线程还在执行，导致内存泄漏或者异常
 	class TaskSys {
 	public:
 		void Init() {
 			m_maxWorkers = std::thread::hardware_concurrency();
 			for (int i = 1; i < m_maxWorkers; i++) {
-				m_workers.push_back(Worker([]() {
-
-				}));
+				m_workers.push_back(Worker());
 			}
 		}
 
 		template <typename Task_t>
-		void Exec(TaskGroup<Task_t>& group) {
+		void Exec(TaskGroup& group) {
 			for (auto it = m_freeWorkerNos.begin(); it != m_freeWorkerNos.end(); it++) {
 				m_workers[it].detach();
 			}
@@ -45,7 +53,12 @@ namespace valkyr {
 		int m_maxWorkers;
 
 	private:
+		void doWork() {
+
+		}
+
 		Vec<Worker> m_workers;
+		Vec<bool> m_workerIsFree;
 		Vec<int> m_freeWorkerNos;
 
 
